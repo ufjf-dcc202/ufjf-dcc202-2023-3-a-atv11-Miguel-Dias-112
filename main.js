0a5e39a24f346a8d00e736b3e6d8404c4cba9729
Miguel Dias <miguel.dias@estudante.ufjf.br>
2023-11-29 20:05:08 -0300

    js adicionados

---
+++ main.js
@@ -0,0 +1,8 @@
+//main.js
+import { getLista } from "./lista"
+const pEntrada = document.querySelector('#entrada')
+const olSaida = document.querySelector('#itens')
+const btnAdicionar = document.querySelector('#adicionar')
+const btnLimpar = document.querySelector('#limpar')
+
+olSaida.innerHTML="<h1>boo<h1>"
