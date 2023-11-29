995ce1468ca612257e2e94f0fd4bdc9c7a0023b3
Miguel Dias <miguel.dias@estudante.ufjf.br>
2023-11-29 20:23:30 -0300

    atualizar lista arrumada

--- main.js
+++ main.js
@@ -5,10 +5,12 @@ const olSaida = document.querySelector('#itens')
 const btnAdicionar = document.querySelector('#adicionar')
 const btnLimpar = document.querySelector('#limpar')
 
-olSaida.innerHTML=""
 function atualizarItemDeLista(){
     const lista = getLista()
+    olSaida.innerHTML=""
+
     for (let i = 0; i < lista.length; i++) {
+
         const item = lista[i]
         const li  = document.createElement('li')
         li.textContent = item
